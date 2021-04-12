27021599695889ed89f353c1850d8bbcd0206739
lzzarlin <63822013@qq.com>
2021-04-12 02:38:54 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,12 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
     public function create()
     {
         return view('sessions.create');
@@ -20,7 +26,8 @@ class SessionsController extends Controller
         ]);
         if (Auth::attempt($credentials, $request->has('remember'))) {
             session()->flash('success', 'welcome back!');
-            return redirect()->route('users.show', [Auth::user()]);
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         } else {
             session()->flash('danger', 'sorry! the email or password is not correct');
             return redirect()->back()->withInput();
