a202e3a9ee060ba35bba6a5a530303c73d3dc040
lzzarlin <63822013@qq.com>
2021-04-12 00:39:49 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', 'welcome back!');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', 'sorry! the email or password is not correct');
+            return redirect()->back()->withInput();
+        }
+    }
+
+    public function destroy()
+    {
+        return view('sessions.destroy');
+    }
+}
