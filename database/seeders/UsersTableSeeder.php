dcb8b981176b50255da8befaed3a2d98b6c53850
lzzarlin <63822013@qq.com>
2021-04-12 03:24:01 +0000

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Lzzarlin';
         $user->email = '63822013@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -2,5 +2,13 @@
   <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png"
       alt="{{ $user->name }}" class="mr-3">
   <a href="{{ route('users.show', $user) }}">
-      {{ $user->name }}</a>
+      {{ $user->name }}
+  </a>
+  @can('destroy', $user)
+  <form action="{{ route('users.destroy', $user->id) }}" method="post" class="float-right">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}
+    <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+  </form>
+  @endcan
 </div>
