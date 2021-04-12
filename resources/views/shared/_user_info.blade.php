aa1a2b1a38b32952d4264e39241e3500fb62ade0
lzzarlin <63822013@qq.com>
2021-04-12 00:12:16 +0000

    完成用户注册功能

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ route('users.show', $user->id) }}">
-  <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
+  <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="{{ $user->name }}" class="gravatar"/>
 </a>
 <h1>{{ $user->name }}</h1>
